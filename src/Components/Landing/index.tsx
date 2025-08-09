05b62a63db9350addc461f840811cf4c880e4451
AfzalYellapur <afzy.278@gmail.com>
2025-08-10 00:54:09 +0530

    misc

--- src/Components/Landing/index.tsx
+++ src/Components/Landing/index.tsx
@@ -6,14 +6,16 @@ import GlassyChatbox from "../ui/GlassyChatBox";
 function Landing() {
     return (
         <div
-        className="flex flex-col items-center justify-center h-screen w-screen absolute top-0 left-0">
+            className="flex flex-col items-center justify-center h-screen w-screen absolute top-0 left-0">
             <LEDMatrix />
             <div className="absolute inset-0 bg-[radial-gradient(ellipse_at_center,rgba(0,0,0,0.95)_0%,rgba(0,0,0,0.9)_5%,rgba(0,0,0,0.85)_15%,rgba(0,0,0,0.8)_20%,rgba(0,0,0,0)_75%)] -z-9" />
             <p className="font-pixelify font-bold text-5xl text-cyan-50 mb-4">
                 What do you want to Manimate?
             </p>
-            <p className=" font-pixelify font-semibold text-xl text-neutral-300/90 mb-5">Turn complex concepts into easy-to-grasp visuals</p>
-            <GlassyChatbox/>
+            <p className=" font-pixelify font-semibold text-xl text-neutral-300/90 mb-5">
+                Turn complex concepts into easy-to-grasp visuals
+            </p>
+            <GlassyChatbox />
         </div>
     );
 }
