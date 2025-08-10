2eb9bcc2afff3c32c6e4fde5a482c7c71757ec6f
AfzalYellapur <afzy.278@gmail.com>
2025-08-11 03:59:44 +0530

    login and signup pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import { Routes, Route } from 'react-router-dom'
 import Landing from './Components/Landing/index'
 import Workspace from './Components/Workspace/index'
-import LoginPage from './Components/Auth/Login'
-import SignupPage from './Components/Auth/Signup'
+import SignPage from './Components/Auth/SigninPage'
+import SignupPage from './Components/Auth/SignupPage'
 
 function App() {
   return (
     <Routes>  
       <Route path="/" element={<Landing />} />
-      <Route path="/login" element={<LoginPage/>} />
+      <Route path="/login" element={<SignPage/>} />
       <Route path="/signup" element={<SignupPage />} />
       <Route path='/workspace' element={<Workspace />} />
     </Routes>
