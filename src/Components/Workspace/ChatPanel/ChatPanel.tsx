7568c0b4c4983070a22d1797a45e4efedc876b33
Eshan A. Bhat <114558660+eshubhat@users.noreply.github.com>
2025-08-15 20:53:27 +0530

    Revert "setting up api"

--- src/Components/Workspace/ChatPanel/ChatPanel.tsx
+++ src/Components/Workspace/ChatPanel/ChatPanel.tsx
@@ -1,31 +1,12 @@
 // import type { Message } from '../types/index';
-import { useState } from "react";
-import GlassyChatbox from "../../ui/GlassyChatBox";
-import GlassyChatInterface from "../../ui/GlassyChatInterface";
+import GlassyChatbox from '../../ui/GlassyChatBox';
+import GlassyChatInterface from '../../ui/GlassyChatInterface';
 
-interface ChatDetails {
-  sessionId: string;
-  sender: string;
-  message: string;
-}
-
-type ChatPanelProps = {
-  setPrompt: (prompt: string) => void;
-  prompt: string;
-  onClick: (message?: string) => Promise<void>;
-};
-
-export default function ChatPanel({
-  setPrompt,
-  prompt,
-  onClick,
-}: ChatPanelProps) {
-  const [chatDetails, setChatDetails] = useState<ChatDetails[]>();
-
+export default function ChatPanel() {
   return (
     <div className="w-[30%] p-2 flex flex-col">
       <GlassyChatInterface />
-      <GlassyChatbox prompt={prompt} setPrompt={setPrompt} onClick={onClick} />
+      <GlassyChatbox />
     </div>
   );
 }
